8396c1b4476942056d7d3f8eb7fe3038a9322c33
davidwanlongwu <davidwu.web@gmail.com>
2025-08-14 13:22:28 -0600

    layout static rendering, avatar loading effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import SignInButtonClient from "@/components/client/signin-button";
-import SignOutButtonClient from "@/components/client/signout-button";
-import UserInfoClient from "@/components/client/user-info";
-import SignInButton from "@/components/sign-button";
-import SignOutButton from "@/components/signout-button";
-import UserInfo from "@/components/user-info";
 import React from "react";
 
 export default function Page() {
-  return (
-    <div>
-      <UserInfo />
-      <SignInButton />
-      <SignOutButton />
-      <hr />
-      <SignInButtonClient />
-      <SignOutButtonClient />
-      <UserInfoClient />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,11 @@
-import { auth, signIn, signOut } from "@/auth";
-import * as actions from "@/actions";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
-  Button,
   Input,
-  Avatar,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
 } from "@heroui/react";
+import HeaderAuth from "./header-auth";
 
 export const AcmeLogo = () => {
   return (
@@ -27,46 +21,6 @@ export const AcmeLogo = () => {
 };
 
 export default async function Header() {
-  const session = await auth();
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = (
-      <Popover placement="bottom">
-        <PopoverTrigger>
-          <Avatar
-            src={
-              session.user.image ||
-              "https://i.pravatar.cc/150?u=a042581f4e29026024d"
-            }
-          />
-        </PopoverTrigger>
-        <PopoverContent>
-          <form action={actions.signOut}>
-            <button type="submit">Log Out</button>
-          </form>
-        </PopoverContent>
-      </Popover>
-    );
-  } else {
-    authContent = (
-      <>
-        <NavbarItem className="hidden lg:flex">
-          <form className="p-4" action={actions.signIn}>
-            <Button type="submit" color="primary" href="#" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" href="#">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
   return (
     <Navbar className="border-b-1 border-gray-200">
       <NavbarBrand>
@@ -78,7 +32,9 @@ export default async function Header() {
           <Input />
         </NavbarItem>
       </NavbarContent>
-      <NavbarContent justify="end">{authContent}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
